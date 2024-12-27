f35792d66a9a7e7bd231e13ae8c7924e09466195
techie_joe <kelechijoseph985@gmail.com>
2024-12-27 15:41:31 +0100

    more adjustment

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';  
 import { WeatherModule } from './modules/weather.module';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    HttpModule,
     ConfigModule.forRoot({
       isGlobal: true,
     }),
